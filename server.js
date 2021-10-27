a13b92e47d34bde3388bec9c0a3d859d02a28e95
Jens Billing <jensis92@hotmail.com>
2021-10-27 11:48:28 +0200

    add endpoint for creating new user, that saves to DB (only accepts numbers for some reason)

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const httpPort = 3000
+const cors = require('cors')
+const db = require("./database.js")
+app.use(cors())
+app.use(express.static('public'))
+const  bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(httpPort, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",httpPort))
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// DB only accepts numbers (bug)
+app.post("/createuser", (req, res, next) => {
+    let newUsername = req.body.username
+    let newPassword = req.body.password
+    let newEmail = req.body.email
+
+    let sql = `INSERT INTO users_table (user_name, user_password, user_email) 
+    VALUES (${newUsername}, ${newPassword}, ${newEmail});`
+
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "quiz":rows
+        })
+    });
+});
+
+// For testing purposes
+app.post("/test", (req, res, next) => {
+    let request= req.body
+    res.send("username : " + req.body.username)
+
+});
